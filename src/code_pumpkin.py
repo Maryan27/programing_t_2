a6cec2e4cee5cd8fd8d28fc3a1afabdfef8a8e13
Maryan27 <151569278+Maryan27@users.noreply.github.com>
2024-03-20 15:03:59 +0200

    code_pumpkin.py

--- src/code_pumpkin.py
+++ src/code_pumpkin.py
@@ -1,18 +1,18 @@
 def generate_matrix(cov, rov):
     matrix = []
-    for i in range(rov):
-        if i % 2 == 0:
-            for j in range(cov):
-                matrix.append((i, j))
+    for j in range(cov):
+        if j % 2 == 0:
+            for i in range(rov):
+                matrix.append((j, i))
         else:
-            for j in range(cov - 1, -1, -1):
-                matrix.append((i, j))
+            for i in range(rov - 1, -1, -1):
+                matrix.append((j, i))
     return matrix
 
-def calculate_seeds_to_plant(m, n,  pumpkin_counts):
-    matrix = generate_matrix(m, n)
+def calculate_seeds_to_plant(cov, rov,  pumpkin_counts):
+    matrix = generate_matrix(cov, rov)
     seeds_to_plant = []
     for pos in matrix:
-        i, j = pos
-        seeds_to_plant.append(pumpkin_counts[i][j])
+        j, i = pos
+        seeds_to_plant.append(pumpkin_counts[j][i])
     return seeds_to_plant
