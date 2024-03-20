3500972fcf056d7334c88122eaed5d0dd4086add
Maryan27 <151569278+Maryan27@users.noreply.github.com>
2024-03-20 16:44:06 +0200

    test_sum_left_node.py

--- test/test_sum_left_node.py
+++ test/test_sum_left_node.py
@@ -1,6 +1,6 @@
 import unittest
-from laba3level2 import branchSums
-from laba3level2 import BinaryTree
+from sum_left_node import branchSums
+from sum_left_node import BinaryTree
 
 
 class TestBranchSums(unittest.TestCase):
@@ -9,11 +9,19 @@ class TestBranchSums(unittest.TestCase):
         root.left = BinaryTree(5)
         root.left.left = BinaryTree(7)
         root.right = BinaryTree(10)
-        self.assertEqual(branchSums(root), 5 + 7)
+        self.assertEqual(branchSums(root), 10)
+    
+    def test_right_children(self):
+        root = BinaryTree(3)
+        root.left = BinaryTree(5)
+        root.right = BinaryTree(7)
+        root.right.right = BinaryTree(10)
+        self.assertEqual(branchSums(root), 17)
 
     def test_single_root(self):
         root = BinaryTree(5)
         self.assertEqual(branchSums(root), 0)
 
+       
 if __name__ == '__main__':
     unittest.main()
