591797d294864ffebf3d52c7e7d2ff266169dc8f
Maryan27 <151569278+Maryan27@users.noreply.github.com>
2024-04-18 23:24:32 +0300

    Update test_sum_left_node.py

--- test/test_sum_left_node.py
+++ test/test_sum_left_node.py
@@ -9,14 +9,14 @@ class TestBranchSums(unittest.TestCase):
         root.left = BinaryTree(5)
         root.left.left = BinaryTree(7)
         root.right = BinaryTree(10)
-        self.assertEqual(branchSums(root), 10)
+        self.assertEqual(branchSums(root), 12)
     
     def test_right_children(self):
         root = BinaryTree(3)
         root.left = BinaryTree(5)
         root.right = BinaryTree(7)
         root.right.right = BinaryTree(10)
-        self.assertEqual(branchSums(root), 17)
+        self.assertEqual(branchSums(root), 5)
 
     def test_single_root(self):
         root = BinaryTree(5)
