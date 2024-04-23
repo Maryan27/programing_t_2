20d1c8e2dc20fb047ac70fcbac5fa709cd3170a8
Maryan27 <151569278+Maryan27@users.noreply.github.com>
2024-04-23 10:45:55 +0300

    Update rabin_karp_code.py

--- src/rabin_karp_code.py
+++ src/rabin_karp_code.py
@@ -1,24 +1,19 @@
 def rabin_karp(haystack, needle, q):
-    n = len(haystack)
-    m = len(needle)
+    text = len(haystack)
+    pattern = len(needle)
     d = 10
     hash_haystack = 0
     hash_needle = 0
-    h = d**(m-1) % q  
+    h = d**(pattern-1) % q  
     
-    for i in range(m):
+    for i in range(pattern):
         hash_needle = (d * hash_needle + ord(needle[i])) % q
         hash_haystack = (d * hash_haystack + ord(haystack[i])) % q
 
-    for i in range(n - m + 1):
+    for i in range(text - pattern + 1):
         if hash_haystack == hash_needle:
-            result = True
-            for j in range(m):
-                if needle[j] != haystack[i + j]:
-                    result = False
-                    break
-            if result:
+            if haystack[i:i+pattern] == needle:
                 return i  
-        if i < n - m:
-            hash_haystack = (d * (hash_haystack - ord(haystack[i]) * h) + ord(haystack[i + m])) % q
-    return -1  
+        if i < text - pattern:
+            hash_haystack = (d * (hash_haystack - ord(haystack[i]) * h) + ord(haystack[i + pattern])) % q
+    return -1
