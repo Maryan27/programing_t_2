afe2679fbe0286f98cee0efb53e89dffca8e238c
Maryan27 <151569278+Maryan27@users.noreply.github.com>
2024-05-09 15:51:50 +0300

    Update heap_based_priority_queue.py

--- src/heap_based_priority_queue.py
+++ src/heap_based_priority_queue.py
@@ -12,19 +12,19 @@ class PriorityQueue:
         self.heap.append(node)  
         self._heapify_up(len(self.heap) - 1)  
 
-    def remove_max(self):
+    def remove_min(self):  
         if not self.heap: 
             return None 
-        max_value = self.heap[0].value  
+        min_value = self.heap[0].value  
         last_node = self.heap.pop()  
         if self.heap: 
             self.heap[0] = last_node 
             self._heapify_down(0)  
-        return max_value  
+        return min_value  
 
     def _heapify_up(self, i):
         parent_i = (i - 1) // 2  
-        while i > 0 and self.heap[parent_i].priority < self.heap[i].priority:
+        while i > 0 and self.heap[parent_i].priority > self.heap[i].priority:  
             self.heap[parent_i], self.heap[i] = self.heap[i], self.heap[parent_i]
             i = parent_i  
             parent_i = (i - 1) // 2  
@@ -32,12 +32,12 @@ class PriorityQueue:
     def _heapify_down(self, i):
         left_child_i = 2 * i + 1  
         right_child_i = 2 * i + 2  
-        largest_i = i  
+        smallest_i = i 
         
-        if left_child_i < len(self.heap) and self.heap[left_child_i].priority > self.heap[largest_i].priority:
-            largest_i = left_child_i
-        if right_child_i < len(self.heap) and self.heap[right_child_i].priority > self.heap[largest_i].priority:
-            largest_i = right_child_i
-        if largest_i != i:  
-            self.heap[largest_i], self.heap[i] = self.heap[i], self.heap[largest_i]
-            self._heapify_down(largest_i)  
+        if left_child_i < len(self.heap) and self.heap[left_child_i].priority < self.heap[smallest_i].priority:  
+            smallest_i = left_child_i
+        if right_child_i < len(self.heap) and self.heap[right_child_i].priority < self.heap[smallest_i].priority:  
+            smallest_i = right_child_i
+        if smallest_i != i:  
+            self.heap[smallest_i], self.heap[i] = self.heap[i], self.heap[smallest_i]
+            self._heapify_down(smallest_i)   
