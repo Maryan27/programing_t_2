5354c7548382bd522077830c1113f60d5d8584c2
Maryan27 <151569278+Maryan27@users.noreply.github.com>
2024-05-09 17:04:33 +0300

    Update test_rabin_karp.py

--- test/test_rabin_karp.py
+++ test/test_rabin_karp.py
@@ -1,3 +1,9 @@
+import os
+import sys
+test_file_path = os.path.abspath(__file__)
+common_parent_path = os.path.abspath(os.path.join(os.path.dirname(test_file_path), os.pardir))
+src_path = os.path.join(common_parent_path, 'src')
+sys.path.append(src_path)
 import unittest
 from rabin_karp import rabin_karp
 
