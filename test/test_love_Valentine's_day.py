aeedf7ba38711d1490460a5e0b56d54faebf18ea
Maryan27 <151569278+Maryan27@users.noreply.github.com>
2024-05-16 19:28:25 +0300

    Update test_love_Valentine's_day.py

--- test/test_love_Valentine's_day.py
+++ test/test_love_Valentine's_day.py
@@ -1,5 +1,5 @@
 import unittest
-from love_Valentine_day import find_three_numbers
+from code_love_Valentine's_day import find_three_numbers
 
 class TestFindThreeNumbers(unittest.TestCase):
     def test_negative_case(self):
@@ -7,13 +7,13 @@ class TestFindThreeNumbers(unittest.TestCase):
         target = 20
         self.assertFalse(find_three_numbers(array, target))
      
-     def test_positive_case(self):
+    def test_positive_case(self):
         array = [1, 2, 3, 4, 5, 6, 8, 9, 7]
         target = 6
         result = find_three_numbers(array, target)
         self.assertTrue(result)
     
-     def test_case(self):
+    def test_case(self):
         array = [20, 2, 31, 4, 35, 89, 57, 23, 39, 90]
         target = 15
         self.assertFalse(find_three_numbers(array, target))
